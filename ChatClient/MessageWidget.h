58e0e4ba694fb98f3803d0573ca88cc03110ce98
Katechkin <nikita.katechkin@office.auriga.msk>
2022-08-01 10:24:31 +0300

    Code style improved

--- ChatClient/MessageWidget.h
+++ ChatClient/MessageWidget.h
@@ -21,17 +21,15 @@ private:
 	void SetTextTypePosition(const COORD& newPos);
 	void MoveOnNextLine();
 
-private:
 	COORD m_textTypePosition = COORD{ 0, 0 };
-
 private:
 	void PrintCharacterToFrame(const wchar_t& character);
 	void DeleteCharacterFromFrame();
 	void PrintText(const std::wstring& text);
 
 private:
+	void MoveBufferOneLineAbove();
 
 public:
-	void MoveBufferOneLineAbove();
 	void PrintMessage(const std::wstring& message);
 };
